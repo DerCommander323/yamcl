ec71962d2c95ab25681c48e21b94bbff2d242269
DerCommander323 <volcarlos323@googlemail.com>
2023-04-25 20:36:27 +0200

    Initial file commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<aside id="sidebar">
+A
+</aside>
+
+<div id="content">
+    <slot />
+</div>
+
+
+<style>
+    #sidebar {
+        background-color: gray;
+        position: fixed;
+        height: 100%;
+        width: auto;
+    }
+</style>
+
+<script>
+    let sidebar = document.querySelector('#sidebar')
+    console.log(sidebar) //logs null
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>YetAnotherMinecraftLauncher</h1>
+
