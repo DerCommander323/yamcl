eaff4023b47e93f8bc4a6c15305162c7052ad363
sudo-CuriousLad <curiousladmc@protonmail.com>
2023-04-27 20:56:16 +0530

    Conditional rendering

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,8 +3,11 @@
     class="left-0 top-0 py-4 items-center flex flex-col absolute h-screen text-white ease-in-out duration-150 p-1 {expanded ? "w-36" : "w-14"}"
     >
     <button 
-        class="w-full rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black" on:mouseover={handleButtonHover} on:mouseout={handleButtonOut} on:focus={handleButtonHover} on:blur={handleButtonOut}>
+        class="w-full flex flex-row rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black" on:mouseover={handleButtonHover} on:mouseout={handleButtonOut} on:focus={handleButtonHover} on:blur={handleButtonOut}>
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="bi bi-house" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/> <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/> </svg>
+        {#if expanded}
+            <div class="grow h-full text-md">Home</div>
+        {/if}
     </button>
     <button 
         class="w-full rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black" on:mouseover={handleButtonHover} on:mouseout={handleButtonOut} on:focus={handleButtonHover} on:blur={handleButtonOut}>
