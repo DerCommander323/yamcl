c60d3169fb4c349ece77472d6ade8b6d14e34d61
DerCommander323 <volcarlos323@googlemail.com>
2023-04-27 19:14:17 +0200

    Wrap Sidebar buttons in div + Change NavButton to slot

--- src/components/NavButton.svelte
+++ src/components/NavButton.svelte
@@ -1,8 +1,7 @@
 <script>
     export let href = '#'
-    export let content = 'Empty?'
 </script>
 
-<a href="{href}" class="w-full rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black">
-    {@html content}
+<a href="{href}" class="w-full h-auto rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black">
+    <slot />
 </a>
