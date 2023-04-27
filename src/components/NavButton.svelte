6f319d2308f834b5b4c1c6251a2197bf98eab281
DerCommander323 <volcarlos323@googlemail.com>
2023-04-27 17:36:03 +0200

    Extract Navbar Button into own component

---
+++ src/components/NavButton.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let href = '#'
+    export let content = 'Empty?'
+</script>
+
+<a href="{href}" class="w-full rounded-md p-2 hover:bg-purple-700 duration-200 fill-white hover:fill-black">
+    {@html content}
+</a>
