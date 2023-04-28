30971fb9a299dc269930d96d801802d689c41ea1
sudo-CuriousLad <curiousladmc@protonmail.com>
2023-04-28 22:19:30 +0530

    Not so thicc plus.

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -34,16 +34,9 @@
 </svg> 
         </NavButton>
 
-<NavButton text="Create" show={expanded}>
-        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	 width="30px" height="30px" viewBox="0 0 349.03 349.031"
-	 xml:space="preserve">
-<g>
-	<path d="M349.03,141.226v66.579c0,5.012-4.061,9.079-9.079,9.079H216.884v123.067c0,5.019-4.067,9.079-9.079,9.079h-66.579
-		c-5.009,0-9.079-4.061-9.079-9.079V216.884H9.079c-5.016,0-9.079-4.067-9.079-9.079v-66.579c0-5.013,4.063-9.079,9.079-9.079
-		h123.068V9.079c0-5.018,4.069-9.079,9.079-9.079h66.579c5.012,0,9.079,4.061,9.079,9.079v123.068h123.067
-		C344.97,132.147,349.03,136.213,349.03,141.226z"/>
-</g>
+        <NavButton text="Create" show={expanded}>
+            <svg width="30px" height="30px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none">
+  <path fill-rule="evenodd" d="M9 17a1 1 0 102 0v-6h6a1 1 0 100-2h-6V3a1 1 0 10-2 0v6H3a1 1 0 000 2h6v6z"/>
 </svg>
     </NavButton>
     </div>
