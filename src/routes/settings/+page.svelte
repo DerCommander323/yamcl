5afed2c1f0a2bf66ade51cd2fea9c4a093d51d20
sudo-CuriousLad <curiousladmc@protonmail.com>
2023-05-05 22:33:56 +0530

    Extracted the file picker into a button.

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,6 +1,11 @@
 Settings
 
+<button on:click={pickDir}>
+    Test
+</button>
+
+
 <script>
     //TODO: Add a way for the user to select the instance folder
-    import './folder_selector'
+    import { pickDir } from './folder_selector';
 </script>
--- src/routes/settings/folder_selector.js
+++ src/routes/settings/folder_selector.js
@@ -1,15 +1,18 @@
 import { open } from '@tauri-apps/api/dialog';
-import { appDir } from '@tauri-apps/api/path';
 // Open a selection dialog for directories
-const selected = await open({
-  directory: true,
-  multiple: true,
-  defaultPath: await appDir(),
-});
-if (Array.isArray(selected)) {
-  // user selected multiple directories
-} else if (selected === null) {
-  // user cancelled the selection
-} else {
-  // user selected a single directory
+
+ export async function pickDir() {
+    const selected = await open({
+        directory: true,
+        multiple: true,
+    });
+    if (Array.isArray(selected)) {
+        // user selected multiple directories
+    } else if (selected === null) {
+        // user cancelled the selection
+    } else {
+        // user selected a single directory
+    }
 }
+
+
