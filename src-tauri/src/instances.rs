6b7fa052a423e54c196bc1ac245472f9ff6e3ed5
sudo-CuriousLad <curiousladmc@protonmail.com>
2023-08-05 21:15:26 +0530

    Preliminary MMC name checking

--- src-tauri/src/instances.rs
+++ src-tauri/src/instances.rs
@@ -1,8 +1,10 @@
 use std::fs::ReadDir;
 use std::fs::read_to_string;
+use std::io::BufRead;
+use std::io::BufReader;
 
 
-fn get_instance_name_cf(dir: ReadDir) {
+pub fn get_instance_name_cf(dir: ReadDir) {
     let mut name = "";
     
     for file in dir {
@@ -15,4 +17,22 @@ fn get_instance_name_cf(dir: ReadDir) {
         }
     }
     println!("{}", name)
+}
+
+fn get_instance_name_mmc(dir: ReadDir) {
+    let mut reader: BufReader<std::fs::File>;
+    let mut name = String::new();
+
+    for file in dir {
+        if file.unwrap().file_name() == "instance.cfg" {
+            reader = BufReader::new(std::fs::File::open(file.unwrap().path()).unwrap())
+            let mut lines = reader.lines();
+            if lines.nth(0).unwrap().unwrap() == "[General]".to_string() {
+                name = lines.nth(32).unwrap().unwrap()
+            } else {
+                name = lines.nth(31).unwrap().unwrap()
+            }
+            
+        }
+    }
 }
