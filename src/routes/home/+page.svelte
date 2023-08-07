4eb2b5f4ecc86a34804d5734dde12c510e3df2e9
DerCommander323 <volcarlos323@googlemail.com>
2023-08-07 21:41:54 +0200

    change fallback icon, add prism icon detection

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -8,15 +8,22 @@
 </div>
 
 <script>
-    import { invoke, convertFileSrc } from "@tauri-apps/api/tauri";
-    import { join } from "@tauri-apps/api/path";
+    import { invoke, convertFileSrc } from "@tauri-apps/api/tauri"
+    import { join } from "@tauri-apps/api/path"
     import { listen } from "@tauri-apps/api/event"
     import { onMount, onDestroy } from "svelte"
 
     import { getSetting } from "../../scripts/settings"
-    import InstanceTile from "../../components/InstanceTile.svelte";
-  import { json } from "@sveltejs/kit";
+    import InstanceTile from "../../components/InstanceTile.svelte"
 
+    const prismIcons = [
+        'default', 'bee', 'brick', 'chicken', 'creeper', 'diamond', 'dirt', 'enderman', 'enderpearl', 'flame', 'fox', 'gear', 'herobrine',
+        'gold', 'grass', 'iron', 'magitech', 'meat', 'modrinth', 'netherstar', 'planks', 'prismlauncher', 'squarecreeper', 'steve', 
+        'stone', 'tnt', 'bee_legacy', 'brick_legacy', 'chicken_legacy', 'creeper_legacy', 'diamond_legacy', 'dirt_legacy',
+        'enderman_legacy', 'enderpearl_legacy', 'flame_legacy', 'fox_legacy', 'gear_legacy', 'herobrine_legacy', 'gold_legacy', 'grass_legacy', 
+        'iron_legacy', 'magitech_legacy', 'meat_legacy', 'modrinth_legacy', 'netherstar_legacy', 'planks_legacy', 'prismlauncher_legacy',
+        'squarecreeper_legacy', 'steve_legacy', 'stone_legacy', 'tnt_legacy'
+    ]
 
     /**
      * @type {import("@tauri-apps/api/event").UnlistenFn}
@@ -39,21 +46,25 @@
         invoke('unlock_icons', { path: iconPath })
         
         unlisten = await listen('instance_create', async (event) => {
-            if(event.payload.icon=='' || event.payload.icon=='default') {
+            let ic = event.payload.icon
+
+            if(ic=='' || prismIcons.includes(ic)) {
                 event.payload.icon = null
-            } else if(event.payload.icon.startsWith("https://media.forgecdn.net")) {
+            } else if(ic == 'flame') {
+                event.payload.icon = 'https://static-beta.curseforge.com/images/favicon.ico'
+            } else if(ic.startsWith("https://media.forgecdn.net")) {
                 //do nothing
-            } else if(event.payload.icon == 'curse:666') {
+            } else if(ic == 'curse:666') {
                 event.payload.icon = null
-            } else if(event.payload.icon.startsWith('curse:') && event.payload.icon != 'curse:666') {
-                let apiReqeust = await fetch(`https://curserinth-api.kuylar.dev/v2/project/${event.payload.icon.split(':')[1]}`)
-                console.log(`Fetching Icon for project ID ${event.payload.icon.split(':')[1]} from CurseRinth...`)
+            } else if(ic.startsWith('curse:') && ic != 'curse:666') {
+                let apiReqeust = await fetch(`https://curserinth-api.kuylar.dev/v2/project/${ic.split(':')[1]}`)
+                console.log(`Fetching Icon for project ID ${ic.split(':')[1]} from CurseRinth...`)
                 apiReqeust.json().then(json => {
                     console.log(json.icon_url)
                     event.payload.icon = json.icon_url
                 })
             } else {
-                event.payload.icon = convertFileSrc(await join(iconPath, event.payload.icon))
+                event.payload.icon = convertFileSrc(await join(iconPath, ic))
             }
             // @ts-ignore
             instanceList = [...instanceList, event.payload]
